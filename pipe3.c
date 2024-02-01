6ce397c6ca3f29a905d025fa6468d345017614e2
1ogolepsy <specialnormal@naver.com>
2024-02-01 16:10:38 +0900

    day01 v0.4 end

---
+++ pipe3.c
@@ -0,0 +1,45 @@
+#include<signal.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<limits.h>
+#include<stdio.h>
+
+int count;
+void alrm_action(int);
+
+void main(){
+	int p[2];
+	int pipe_size;
+	char c = 'x';
+	static struct sigaction act;
+	act.sa_handler = alrm_action;
+	sigfillset(&(act.sa_mask));
+
+	if(pipe(p) == -1){
+		perror("pipe call");
+		exit(1);
+	}
+	
+	pipe_size = fpathconf(p[0], _PC_PIPE_BUF);
+	printf("Maximum size of write to pipe : %d bytes \n", pipe_size);
+
+	sigaction(SIGALRM, &act, NULL);
+
+	while(1){
+//		alarm(1);
+		write(p[1], &c, 1);
+//		alarm(0);
+
+		if((++count % 1024) == 0)
+			printf("%d characters in pipe \n", count);
+		
+		if(pipe_size == count)
+			alarm(0);
+//			break;
+	}
+}
+
+void alrm_action(int signo){
+	printf("write blocked after %d aharacters \n", count);
+	exit(0);
+}
